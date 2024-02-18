960491ead4cae01b4d212d7d5b1e013ab6c1039d
codingmagiczh <codingmagiczh@163.com>
2024-02-18 09:34:55 +0800

    test: 添加字符串 String 类单元测试

---
+++ src/test/java/lang/StringTest.java
@@ -0,0 +1,12 @@
+package test.java.lang;
+
+/**
+ * 字符串测试类
+ *
+ * @author Zhang Guoping
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        System.out.println("Hello JDK Source!");
+    }
+}
